bc1f72ff75fcfafcf08ee6dc2f9aafe66b65cc13
username <useremail>
2024-10-20 16:49:48 -0700

    Guessing game complete

--- GuessingGame/GuessingGame.py
+++ GuessingGame/GuessingGame.py
@@ -1,1 +1,14 @@
-
+print("Guess the number!")
+import random
+n = random.randint(1, 99)
+guess = int(input("Enter an interger from 1 to 99:   "))
+while True:
+     if guess < n:
+         print("Guess is too low!")
+         guess = int(input("Enter an interger from 1 to 99:   "))
+     elif guess > n:
+         print("Guess is too high!")
+         guess = int(input("Enter an interger from 1 to 99:   "))
+     else:
+         print("You guessed it right!")
+         break
